4d90c309b832e9047330bbfc2c28c49dc8622996
dmanido <dmanido@there>
2022-10-01 00:56:50 +0200

    Scale Earth model to its real-life size in km

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ npm start
 - [ ] Render ISS in current position
 - [ ] Render ISS predicted orbit
 - [ ] Correct sun position (currently based on UTC time alone)
+- [ ] Add starry background
 
 ---
 
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
 import { SettingsOverlay } from './SettingsOverlay'
 import { degToRad, noop } from './utils'
-import { getLatLngObj } from "tle.js"
+import { getLatLngObj, getSatelliteInfo } from 'tle.js'
 
 const ORIGIN = new THREE.Vector3(0, 0, 0)
 const Y_AXIS = new THREE.Vector3(0, 1, 0)
 const SUN_AT_NULL_ISLAND = new THREE.Vector3(0, 0, -5_000)
+
+const EARTH_DIAMETER_POLES_KM = 12_714
+const EARTH_DIAMETER_EQUATOR_KM = 12_756
 
 const MS_IN_HOUR = 60 * 60 * 1_000
 const MS_IN_DAY = 24 * MS_IN_HOUR
@@ -32,42 +35,30 @@ scene.add(ambientLight)
 const sunLight = new THREE.DirectionalLight(0xFFCC99, 10)
 scene.add(sunLight)
 
-const axesHelper = new THREE.AxesHelper(1_000)
+const axesHelper = new THREE.AxesHelper(EARTH_DIAMETER_EQUATOR_KM)
 scene.add(axesHelper)
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1_000)
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, Number.MAX_SAFE_INTEGER)
+camera.position.x = EARTH_DIAMETER_EQUATOR_KM
+camera.position.y = EARTH_DIAMETER_EQUATOR_KM
+camera.position.z = EARTH_DIAMETER_EQUATOR_KM
+camera.lookAt(ORIGIN)
 
 const orbitControls = new OrbitControls(camera, renderer.domElement)
-//orbitControls.minDistance = 600
-//orbitControls.maxDistance = 2_000
+orbitControls.minDistance = EARTH_DIAMETER_EQUATOR_KM
+orbitControls.maxDistance = EARTH_DIAMETER_EQUATOR_KM * 3
 
 new GLTFLoader().load('models/Earth_1_12756.glb', gltf => {
-   const modelScene = gltf.scene || gltf.scenes[0]
-   if (!modelScene) {
-      throw new Error('Failed to load model.')
-   }
+   const box = new THREE.Box3().setFromObject(gltf.scene)
+   const center = box.getCenter(new THREE.Vector3)
 
-   const box = new THREE.Box3().setFromObject(modelScene)
-   const size = box.getSize(new THREE.Vector3()).length()
-   const center = box.getCenter(new THREE.Vector3())
+   gltf.scene.scale.setScalar(EARTH_DIAMETER_EQUATOR_KM / 1_000)
+   gltf.scene.position.x += gltf.scene.position.x - center.x
+   gltf.scene.position.y += gltf.scene.position.y - center.y
+   gltf.scene.position.z += gltf.scene.position.z - center.z
 
-   modelScene.position.x += modelScene.position.x - center.x
-   modelScene.position.y += modelScene.position.y - center.y
-   modelScene.position.z += modelScene.position.z - center.z
-
-   camera.near = size / 100
-   camera.far = size * 100
-   camera.updateProjectionMatrix()
-
-   camera.position.copy(center)
-   camera.position.x += size / 2
-   camera.position.y += size / 5
-   camera.position.z += size / 2
-   camera.lookAt(center)
-
-   scene.add(modelScene)
+   scene.add(gltf.scene)
 }, noop, console.error)
-
 
 
 
@@ -109,28 +100,20 @@ document.body.append(settingsOverlay, renderer.domElement)
 
 
 
-
-//fetch('https://celestrak.org/NORAD/elements/gp.php?CATNR=25544&FORMAT=TLE')
-Promise.resolve(`ISS (ZARYA)             
+// fetch('https://celestrak.org/NORAD/elements/gp.php?CATNR=25544&FORMAT=TLE')
+Promise.resolve(`ISS (ZARYA)
 1 25544U 98067A   22271.29875188  .00011405  00000+0  20504-3 0  9996
 2 25544  51.6445 185.7292 0002377 302.6655 202.5977 15.50344723361171`)
    .then(
       tle => {
-         const latLong = getLatLngObj(tle)
-         new FBXLoader().load(
-            'models/ISSComplete1.fbx',
-            (object) => {
-               object.position.z = 1100
-               object.scale.set(10, 10, 10)
-               scene.add(object)
-            },
-            (xhr) => {
-                console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-            },
-            (error) => {
-                console.log(error)
-            }
-        )
+         console.debug('getLatLngObj(tle)', getLatLngObj(tle))
+         console.debug('getSatelliteInfo(tle)', getSatelliteInfo(tle, Date.now()))
+
+         new FBXLoader().load('models/ISSComplete1.fbx', object => {
+            object.position.z = 1100
+            object.scale.set(10, 10, 10)
+            scene.add(object)
+         }, noop, console.error)
       })
 
 render()
