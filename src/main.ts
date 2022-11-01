f1a318e3c612c0f41eb22044623dc676d80874e9
dmanido <dmanido@there>
2022-11-01 12:44:55 +0100

    Make orbit beams bloom

--- src/main.ts
+++ src/main.ts
@@ -8,20 +8,29 @@ import {
    DirectionalLight,
    DodecahedronGeometry,
    GridHelper,
+   Layers,
    Line,
    LineBasicMaterial,
    LineDashedMaterial,
+   Material,
    MathUtils,
    Mesh,
+   MeshBasicMaterial,
    MeshStandardMaterial,
    Object3D,
    PerspectiveCamera,
    Scene,
+   ShaderMaterial,
    SphereGeometry,
+   Vector2,
    Vector3,
    WebGLRenderer
 } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass'
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer'
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass'
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass'
 import { getLatLngObj } from 'tle.js'
 import { h } from './dom'
 import { LocationPredictionOverlay } from './LocationPredictionOverlay'
@@ -45,6 +54,11 @@ import {
 } from './utils'
 
 const { degToRad } = MathUtils
+
+const enum SceneLayer {
+   Default = 0,
+   Bloom = 1
+}
 
 const renderer = new WebGLRenderer({ antialias: true, alpha: true, logarithmicDepthBuffer: true })
 renderer.physicallyCorrectLights = true
@@ -88,6 +102,55 @@ camera.lookAt(ORIGIN)
 const orbitControls = new OrbitControls(camera, renderer.domElement)
 orbitControls.minDistance = EARTH_DIAMETER_EQUATOR_KM * 0.55
 orbitControls.maxDistance = EARTH_DIAMETER_EQUATOR_KM * 3
+
+const vertexShader = `
+varying vec2 vUv;
+
+void main() {
+   vUv = uv;
+   gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}`
+
+const fragmentShader = `
+uniform sampler2D baseTexture;
+uniform sampler2D bloomTexture;
+
+varying vec2 vUv;
+
+void main() {
+   gl_FragColor = texture2D(baseTexture, vUv) + vec4(1.0) * texture2D(bloomTexture, vUv);
+}`
+
+const bloomLayer = new Layers
+bloomLayer.set(SceneLayer.Bloom)
+
+const renderScene = new RenderPass(scene, camera)
+
+const bloomPass = new UnrealBloomPass(new Vector2(window.innerWidth, window.innerHeight), 4, 0.25, 0)
+
+const bloomComposer = new EffectComposer(renderer)
+bloomComposer.renderToScreen = false
+bloomComposer.setSize(window.innerWidth, window.innerHeight) // TODO: Update on window resize.
+bloomComposer.addPass(renderScene)
+bloomComposer.addPass(bloomPass)
+
+const finalPass = new ShaderPass(
+   new ShaderMaterial({
+      uniforms: {
+         baseTexture: { value: null },
+         bloomTexture: { value: bloomComposer.renderTarget2.texture }
+      },
+      vertexShader,
+      fragmentShader,
+      defines: {}
+   }),
+   'baseTexture'
+)
+finalPass.needsSwap = true
+
+const finalComposer = new EffectComposer(renderer)
+finalComposer.addPass(renderScene)
+finalComposer.addPass(finalPass)
 
 const frame = { time: Date.now() }
 let timeShift = 0
@@ -164,11 +227,13 @@ loadGltfModel('assets/iss-lossy.glb').then(gltf => {
 const issFutureOrbitMaterial = new LineBasicMaterial({ color: 0xff0000, transparent: true, opacity: 0.9 })
 const issFutureOrbitGeometry = new BufferGeometry
 const issFutureOrbitLine = new Line(issFutureOrbitGeometry, issFutureOrbitMaterial)
+issFutureOrbitLine.layers.enable(SceneLayer.Bloom)
 scene.add(issFutureOrbitLine)
 
 const issPastOrbitMaterial = new LineDashedMaterial({ color: 0xff00ff, transparent: true, opacity: 0.9, gapSize: 500, dashSize: 500 })
 const issPastOrbitGeometry = new BufferGeometry
 const issPastOrbitLine = new Line(issPastOrbitGeometry, issPastOrbitMaterial)
+issPastOrbitLine.layers.enable(SceneLayer.Bloom)
 scene.add(issPastOrbitLine)
 
 function positionIss() {
@@ -249,7 +314,26 @@ window.addEventListener('resize', () => {
    camera.updateProjectionMatrix()
 
    renderer.setSize(innerWidth, innerHeight)
+   bloomComposer.setSize(innerWidth, innerHeight)
+   finalComposer.setSize(innerWidth, innerHeight)
 })
+
+const materials: Record<string, Material> = {}
+const darkMaterial = new MeshBasicMaterial({ color: 'black' })
+
+function darkenNonBloomed(obj: Object3D) {
+   if (obj instanceof Mesh && !bloomLayer.test(obj.layers)) {
+      materials[obj.uuid] = obj.material
+      obj.material = darkMaterial
+   }
+}
+
+function restoreMaterial(obj: Object3D) {
+   if (materials[obj.uuid]) {
+      (obj as Mesh).material = materials[obj.uuid]
+      delete materials[obj.uuid]
+   }
+}
 
 function render() {
    frame.time = Date.now() + timeShift
@@ -270,7 +354,12 @@ function render() {
    orbitControls.rotateSpeed = normalize(controlsDistance, controlsMinDistance, controlsMaxDistance, 0.02, 1)
    orbitControls.zoomSpeed = normalize(controlsDistance, controlsMinDistance, controlsMaxDistance, 0.25, 1)
 
-   renderer.render(scene, camera)
+   scene.traverse(darkenNonBloomed)
+   bloomComposer.render()
+   scene.traverse(restoreMaterial)
+
+   finalComposer.render()
+
    requestAnimationFrame(render)
 }
 
